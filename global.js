b5f03beaf4bba36746fb4f1f82d4c905ca6e197a
Lucas Daniel <76965620+Lucas-Daniel07@users.noreply.github.com>
2022-12-11 20:53:59 -0300

    new_at

--- global.js
+++ global.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = function(){
     definir_local_storage(true);
 }
@@ -21,27 +20,3 @@ function definir_local_storage(onload=false){
         }
     }
   }
-=======
-window.onload = function(){
-    definir_local_storage(true);
-}
-function definir_local_storage(onload=false){
-    if(!onload){
-    if(localStorage.getItem('menu') == null){
-        localStorage.setItem('menu', 'aberto');
-    } else if(localStorage.getItem('menu') == 'aberto'){
-        document.getElementById('sidebar').classList.remove('show');
-        localStorage.setItem('menu', 'fechado');
-    } else {
-        document.getElementById('sidebar').classList.add('show');
-        localStorage.setItem('menu', 'aberto');
-    }  
-    }else {
-        if(localStorage.getItem('menu') == 'aberto'){
-            document.getElementById('sidebar').classList.add('show');
-        } else {
-            document.getElementById('sidebar').classList.remove('show');
-        }
-    }
-  }
->>>>>>> b272fadbaec194cf080f9ed0a7229b4b6d312778
